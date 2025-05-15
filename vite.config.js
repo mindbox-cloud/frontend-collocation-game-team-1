80dd9c9c48ed5e01cbe95f510cd74967822fbd94
Aleksei Korelin <alex.korelin.dev@gmail.com>
2025-05-15 12:05:16 +0300

    fix: some fix

--- vite.config.js
+++ vite.config.js
@@ -16,6 +16,9 @@ export default defineConfig(() => {
         ],
       }),
     ],
+    server: {
+      port: 3000,
+    },
     // https://vitejs.dev/guide/build.html
     build: {
       target: 'es2019',
