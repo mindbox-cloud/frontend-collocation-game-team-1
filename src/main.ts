45f758454dea5ece51774a48310b11dabc4998c9
Aleksei Korelin <alex.korelin.dev@gmail.com>
2025-05-15 12:10:15 +0300

    feat: add input

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,8 @@
-alert('Hello, world!')
+const container = document.getElementById('app')
+console.log(container)
+if (container) {
+    const sizeInput = document.createElement('input')
+    sizeInput.type = 'number'
+    sizeInput.value = '100'
+    container.appendChild(sizeInput)
+}
