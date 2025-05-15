e6cb758fa953431e3a6f77dd6b9fa3e700b8a7f5
Aleksei Korelin <alex.korelin.dev@gmail.com>
2025-05-15 11:50:14 +0300

    fix: some fix

--- README.md
+++
@@ -1,7 +0,0 @@
-# aaa-game
-
-## Как собрать?
-
-nvm use 18
-npm i
-npm run build
--- jest.config.cjs
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'jsdom',
-  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
-  testMatch: ['**/__tests__/**/*.test.ts'],
-  moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1',
-  },
-}; 
